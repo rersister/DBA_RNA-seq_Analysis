1caa93a213abf59ec3c71fc365a33818b9dd584a
zhenggm <37731785+rersister@users.noreply.github.com>
2024-01-18 17:07:38 +0800

    Update step5_annoVCF_all.sh

--- RNA-seq variant calling analysis/step5_annoVCF_all.sh
+++ RNA-seq variant calling analysis/step5_annoVCF_all.sh
@@ -4,8 +4,8 @@
 #PBS -l nodes=1:ppn=5,mem=50gb,walltime=40:00:00 
 #HSCHED -s hschedd 
 
-wk_path='/xtdisk/fangxd_group/zhenggm/DBA_BulkRNA_10/GATK-RNA-seq2call_variants/Anovar/annovar'
-outdir='/xtdisk/fangxd_group/zhenggm/DBA_BulkRNA_10/GATK-RNA-seq2call_variants/4.variants-calling' #输出文件路径outdir=$5 #输出文件路径
+wk_path='~/DBA_BulkRNA_10/GATK-RNA-seq2call_variants/Anovar/annovar'
+outdir='~/DBA_BulkRNA_10/GATK-RNA-seq2call_variants/4.variants-calling' #输出文件路径outdir=$5 #输出文件路径
 
 
 cat sample.txt |while read id
